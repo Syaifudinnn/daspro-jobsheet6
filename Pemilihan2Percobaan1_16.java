beb5fa38105e1aaefdf8d46cfe4383c0672e4235
Syaifudinnn <mukhammad_adn_30rpl@student.smktelkom-mlg.sch.id>
2024-10-03 13:10:46 +0700

    soal no 4

--- Pemilihan2Percobaan1_16.java
+++ Pemilihan2Percobaan1_16.java
@@ -12,12 +12,17 @@ public class Pemilihan2Percobaan1_16 {
         int tahun = input.nextInt();
 
         //logic
-        if (tahun % 4 == 0) {
-            if (tahun % 100 != 0 && tahun % 400 == 0) {
+        if (tahun % 4 == 0) { //habis dibagi 4
+            if (tahun % 100 != 0) { //tidak habis dibagi 100
                 System.out.println(tahun + " termasuk tahun kabisat");
             }
             else {
-                System.out.println(tahun + " bukan tahun kabisat");
+                if (tahun % 400 == 0) { //habis dibagi 400
+                    System.out.println(tahun + " termasuk tahun kabisat");
+                }
+                else {
+                    System.out.println(tahun + " bukan tahun kabisat");
+                }
             }
         }
         else {
